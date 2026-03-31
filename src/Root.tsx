c3532230b03c4c32e98011674f708ee22a551032
Jeshwanth Shiva Sai <63970360+jeshwanthshivasai@users.noreply.github.com>
2026-03-31 13:13:58 +0530

    feat: add DummyMain component and register as ReNEXT-Preview composition in Root

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Composition } from 'remotion';
+import { DummyMain } from './DummyMain';
 import { Main } from './Main';
 import { FPS, TOTAL_DURATION } from './Constants';
 
 export const RemotionRoot: React.FC = () => {
     return (
         <>
+            {/* The Dummy Main for Sir's Preview */}
+            <Composition
+                id="ReNEXT-Preview"
+                component={DummyMain}
+                durationInFrames={15 * FPS}
+                fps={FPS}
+                width={1920}
+                height={1080}
+            />
+
+            {/* Main high-end production (Hidden for now) */}
+            {/* 
             <Composition
                 id="ReNEXT-Explainer"
                 component={Main}
@@ -13,7 +26,8 @@ export const RemotionRoot: React.FC = () => {
                 fps={FPS}
                 width={1920}
                 height={1080}
-            />
+            /> 
+            */}
         </>
     );
 };
