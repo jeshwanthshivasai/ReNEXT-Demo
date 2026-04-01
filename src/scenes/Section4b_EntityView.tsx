0e0a527fadfd82977886c8fac11c18283479991d
Jeshwanth Shiva Sai <63970360+jeshwanthshivasai@users.noreply.github.com>
2026-04-01 18:13:38 +0530

    chore: remove video ignore patterns and add missing React imports to scene components

--- src/scenes/Section4b_EntityView.tsx
+++ src/scenes/Section4b_EntityView.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ScreenWalkthroughLayout } from '../components/ScreenWalkthroughLayout';
 import { AbsoluteFill, interpolate, useCurrentFrame, staticFile } from 'remotion';
 
